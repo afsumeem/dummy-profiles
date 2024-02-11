c30d8c7258af6f682e94ad466bcb599eb5b8c979
Afsana Meem <afsumeem@gmail.com>
2024-02-12 00:28:54 +0600

    sorting fucntionality done

--- src/components/UI/AllUsers/AllUsers.tsx
+++ src/components/UI/AllUsers/AllUsers.tsx
@@ -8,6 +8,7 @@ import { CiSearch } from "react-icons/ci";
 const AllUsers = () => {
   const [users, setUsers] = useState<UserData[]>([]);
   const [searchUsers, setSearchUsers] = useState<string>("");
+  const [sortBy, setSortBy] = useState<string>("");
 
   // fetch user data
 
@@ -35,21 +36,54 @@ const AllUsers = () => {
     user.firstName.toLowerCase().includes(searchUsers.toLowerCase())
   );
 
+  // Sort users based on selected name, email and company
+
+  const sortedUsers = filteredUsers.sort((user1, user2) => {
+    if (sortBy === "name") {
+      return user1.firstName.localeCompare(user2.firstName);
+    } else if (sortBy === "email") {
+      return user1.email.localeCompare(user2.email);
+    } else if (sortBy === "company") {
+      return user1.company.name.localeCompare(user2.company.name);
+    }
+    return 0;
+  });
+
+  //
+
   return (
     <div>
       <h1 className="mt-4 mb-2 text-center">ALL USERS</h1>
       <hr />
 
-      <div className="w-25 ms-auto">
-        <InputGroup className="mb-3 ">
+      {/* search input  */}
+      <div className="d-flex flex-column flex-md-row justify-content-between align-items-center mx-4 gap-4 gap-md-1">
+        {/* sorting */}
+        <Form.Select
+          aria-label="Select sorting option"
+          value={sortBy}
+          onChange={(event) => setSortBy(event.target.value)}
+        >
+          <option>All</option>
+          <option value="name">Sort by name</option>
+          <option value="email">Sort by email</option>
+          <option value="company">Sort by Company name</option>
+        </Form.Select>
+
+        {/*  search input*/}
+        <InputGroup className="mb-3 w-25">
           <Form.Control
-            placeholder="Recipient's username"
-            aria-label="Recipient's username"
+            placeholder="Search users"
+            className="border-0 border-bottom rounded-0 "
+            aria-label="Search users"
             aria-describedby="basic-addon2"
             value={searchUsers}
             onChange={(e) => setSearchUsers(e.target.value)}
           />
-          <Button variant="outline-secondary" id="button-addon2">
+          <Button
+            className="bg-transparent text-dark border-0 border-bottom rounded-0"
+            id="button-addon2"
+          >
             <CiSearch />
           </Button>
         </InputGroup>
@@ -68,10 +102,18 @@ const AllUsers = () => {
           </div>
         ) : (
           <>
-            {filteredUsers.length === 0 ? (
-              <h2 className="text-center w-100 text-danger">No users found.</h2>
+            {users.length === 0 ? (
+              <div className="d-block mx-auto mt-5">
+                <Spinner animation="border" variant="dark" />
+              </div>
             ) : (
-              filteredUsers.map((user) => <User key={user.id} user={user} />)
+              <>
+                {sortedUsers.length === 0 ? (
+                  <div className="text-center w-100">No users found.</div>
+                ) : (
+                  sortedUsers.map((user) => <User key={user.id} user={user} />)
+                )}
+              </>
             )}
           </>
         )}
