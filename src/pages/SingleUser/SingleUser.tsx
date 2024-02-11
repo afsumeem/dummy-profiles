101c2c68b95db30252b5e523e7f4e231abece987
Afsana Meem <afsumeem@gmail.com>
2024-02-11 16:36:21 +0600

    display single user data

--- src/pages/SingleUser/SingleUser.tsx
+++ src/pages/SingleUser/SingleUser.tsx
@@ -1,12 +1,31 @@
 import { useLoaderData } from "react-router-dom";
 import { UserData } from "../../types";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
 
 const SingleUser: React.FC = () => {
   const user: UserData = useLoaderData() as UserData;
-  const { firstName } = user;
+  const { id, firstName, lastName, image, email, address, company } = user;
   return (
     <div>
-      <h1>{firstName}</h1>
+      <Col key={id}>
+        <Card>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>
+              {firstName}
+              {lastName}
+            </Card.Title>
+            <Card.Text>
+              {email}
+              {address.address}
+              {address.city}
+              {address.state}
+              {company.name}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
     </div>
   );
 };
